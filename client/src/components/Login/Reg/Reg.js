d76c4f39cf7fb7f86b19b4071dc8f38e026ae018
Shamany4 <anton-shamov99@mail.ru>
2020-04-07 13:18:48 +0700

    Внесение правок

--- client/src/components/Login/Reg/Reg.js
+++ client/src/components/Login/Reg/Reg.js
@@ -3,6 +3,7 @@ import "../login.sass"
 import {Link} from "react-router-dom";
 import axios from "axios";
 import InputMask from 'react-input-mask';
+import { useForm } from 'react-hook-form';
 
 const Reg = (props) => {
 
@@ -13,15 +14,17 @@ const Reg = (props) => {
     username: ''
   });
 
+  // Собираем данные в form
   const changeHandler = event => {
     setForm({...form, [event.target.name]:event.target.value })
   }
 
+  // Отправка на сервер
   const registerHandler = () => {
     let number = form.phone;
     number.split(/\W/);
-    let phone = number[1] + number[4] + number[5] + number[6] + number[9]
-      + number[10] + number[11] + number[13] + number[14] + number[15] + number[16];
+    let phone = number[0] + number[3] + number[4] + number[5] + number[8] + number[9]
+          + number[10] + number[12] + number[13] + number[14] + number[15]
     axios({
       url: `http://localhost:5000/api/auth/register`,
       method: 'POST',
@@ -35,19 +38,10 @@ const Reg = (props) => {
       .catch(error => console.log(error))
   }
 
-  let classInput = 'login-form-input__item';
-
-  const validationEmail = (event) => {
-    if (event.target.value !== ' ') {
-      let regex = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
-      regex.test(event.target.value);
-      if (regex.test(event.target.value)) {
-        classInput += ' login-form-input__item_success';
-        console.log(event.target.value);
-        alert('Успех!');
-      }
-    }
-  }
+  const { register, handleSubmit } = useForm(); // инициализация хуков
+  const onSubmit = data => {
+    console.log(data)
+  };
 
   return(
 
@@ -71,11 +65,11 @@ const Reg = (props) => {
                 <input
                   type="email"
                   name="email"
-                  className={classInput}
+                  className="login-form-input__item"
                   placeholder="Ваш email"
-                  // onBlur={validationEmail}
-                  onChange={validationEmail}
-                  // onChange={changeHandler}
+                  ref={register({required: true, pattern: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/})}
+                  onBlur={handleSubmit(onSubmit)}
+                  onChange={changeHandler}
                 />
                 <span className="login-form-input__span"><i className="fas fa-envelope"></i></span>
               </div>
@@ -86,16 +80,17 @@ const Reg = (props) => {
                   name="password"
                   className="login-form-input__item"
                   placeholder="Ваш пароль"
+                  ref={register({required: true, max: 20, min: 8, maxLength: 20, minLength: 8})}
+                  onBlur={handleSubmit(onSubmit)}
                   onChange={changeHandler}
                 />
                 <span className="login-form-input__span"><i className="fas fa-lock"></i></span>
               </div>
             <div className="login-form-input-block">
                 <InputMask
-                  mask="+7 (999) 999 9999"
+                  mask="8 (999) 999 9999"
                   type="text"
                   name="phone"
-                  // maskChar=" "
                   onChange={changeHandler}
                   className="login-form-input__item"
                   placeholder="Ваш телефон"
